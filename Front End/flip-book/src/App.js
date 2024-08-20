93ca3530ca9969f0516ae6b13e4590986a6ab9e9
Muhammad Bilal <bilal119797@gmail.com>
2024-08-20 09:29:14 -0600

    Completed header

--- Front End/flip-book/src/App.js
+++ Front End/flip-book/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header title="Make your own Flip Books" />
+      <Header title="Flip Books" />
     </div>
   );
 }
--- Front End/flip-book/src/Components/Header.js
+++ Front End/flip-book/src/Components/Header.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import "../Styles/Header.css"
+import React from "react";
+import "../Styles/Header.css";
 
 export default function Header(props) {
-  return (
-    <header className="App-header">
-
-      <div className="nav-container">
-        <h1 className="nav-title">{props.title}</h1>
-      </div>
-      
-    </header>
-  );
+	return (
+		<header className="App-header">
+			<nav className="navbar">
+				<div className="nav-title">
+					<h1>{props.title}</h1>
+				</div>
+				<div className="nav-links">
+					<a href="/" className="Home">
+						Home
+					</a>
+          <a href="#" className="Make your own">
+						Make your own
+					</a>
+					<a href="#" className="History">
+						History
+					</a>
+				</div>
+			</nav>
+		</header>
+	);
 }
